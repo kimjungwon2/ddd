9e888f0a4b42cbdcf621b541699eac543b4cb204
jungwon <mac2001035@gmail.com>
2024-05-16 01:49:26 +0900

    FEAT: 주문 도메인 구현.

--- src/main/java/com/myshop/order/command/domain/OrderState.java
+++ src/main/java/com/myshop/order/command/domain/OrderState.java
@@ -1,5 +1,18 @@
 package com.myshop.order.command.domain;
 
 public enum OrderState {
-    PAYMENT_WAITING, PREPARING, SHIPPED, DELIVERING, DELIVERY_COMPLETED, CANCELED
+    PAYMENT_WAITING{
+        public boolean isShippingChangeable(){
+            return true;
+        }
+    }, PREPARING{
+        public boolean isShippingChangeable(){
+            return true;
+        }
+    }, SHIPPED, DELIVERING, DELIVERY_COMPLETED;
+
+    public boolean isShippingChangeable(){
+        return false;
+    }
+
 }
