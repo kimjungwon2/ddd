6c5118644d380ab52396b9d5b3635a364fa647c4
jungwon <mac2001035@gmail.com>
2024-08-11 18:55:44 +0900

    FEAT: Order 도메인에 Shipping 판별.

--- src/main/java/com/myshop/order/command/domain/OrderState.java
+++ src/main/java/com/myshop/order/command/domain/OrderState.java
@@ -1,18 +1,6 @@
 package com.myshop.order.command.domain;
 
 public enum OrderState {
-    PAYMENT_WAITING{
-        public boolean isShippingChangeable(){
-            return true;
-        }
-    }, PREPARING{
-        public boolean isShippingChangeable(){
-            return true;
-        }
-    }, SHIPPED, DELIVERING, DELIVERY_COMPLETED;
-
-    public boolean isShippingChangeable(){
-        return false;
-    }
+    PAYMENT_WAITING, PREPARING, SHIPPED, DELIVERING, DELIVERY_COMPLETED;
 
 }
