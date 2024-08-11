a912ad6319c14f17c2155aabbb2bb3036271b473
jungwon <mac2001035@gmail.com>
2024-08-12 01:44:11 +0900

    FEAT: Order와 OrderLine 관계 설정.

--- src/main/java/com/myshop/order/command/domain/Order.java
+++ src/main/java/com/myshop/order/command/domain/Order.java
@@ -1,7 +1,17 @@
 package com.myshop.order.command.domain;
 
 
+import java.util.List;
+
 public class Order {
+
+    private List<OrderLine> orderLines;
+    private Money totalAmounts;
+
+    public Order(List<OrderLine> orderLines) {
+        setOrderLines(orderLines);
+    }
+
     private OrderState state;
     private ShippingInfo shippingInfo;
 
@@ -25,6 +35,26 @@ public class Order {
     }
 
     public void completePayment(){
+
+    }
+
+    private void setOrderLines(List<OrderLine> orderLines){
+        verifyAtLeastOneOrMoreOrderLines(orderLines);
+        this.orderLines = orderLines;
+        calculateTotalAmounts();
+    }
+
+    private void verifyAtLeastOneOrMoreOrderLines(List<OrderLine> orderLines){
+        if(orderLines == null || orderLines.isEmpty()){
+            throw new IllegalStateException("no OrderLine");
+        }
+    }
+
+    private void calculateTotalAmounts(){
+        int sum = orderLines.stream()
+                .mapToInt(x->x.getAmounts())
+                .sum();
+        this.totalAmounts = new Money(sum);
 
     }
 
