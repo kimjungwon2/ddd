b172cfec616fede8a3fc1bde9bcf16d20d85b068
jungwon <mac2001035@gmail.com>
2024-08-11 19:02:52 +0900

    FEAT: 주문 목록 도메인 구현.

--- src/main/java/com/myshop/order/command/domain/OrderLine.java
+++ src/main/java/com/myshop/order/command/domain/OrderLine.java
@@ -1,57 +1,24 @@
 package com.myshop.order.command.domain;
 
-import com.myshop.catalog.command.domain.product.ProductId;
-import com.myshop.common.jpa.MoneyConverter;
-import com.myshop.common.model.Money;
-
-import javax.persistence.Column;
-import javax.persistence.Convert;
-import javax.persistence.Embeddable;
-import javax.persistence.Embedded;
-
-@Embeddable
 public class OrderLine {
-    @Embedded
-    private ProductId productId;
-
-    @Convert(converter = MoneyConverter.class)
-    @Column(name = "price")
-    private Money price;
-
-    @Column(name = "quantity")
+    private Product product;
+    private int price;
     private int quantity;
+    private int amounts;
 
-    @Convert(converter = MoneyConverter.class)
-    @Column(name = "amounts")
-    private Money amounts;
-
-    protected OrderLine() {
-    }
-
-    public OrderLine(ProductId productId, Money price, int quantity) {
-        this.productId = productId;
+    public OrderLine(Product product, int price, int quantity, int amounts) {
+        this.product = product;
         this.price = price;
         this.quantity = quantity;
-        this.amounts = calculateAmounts();
+        this.amounts = amounts;
     }
 
-    private Money calculateAmounts() {
-        return price.multiply(quantity);
+    private int calculateAmounts(){
+        return price * quantity;
     }
 
-    public ProductId getProductId() {
-        return productId;
-    }
-
-    public Money getPrice() {
-        return price;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public Money getAmounts() {
+    public int getAmounts(){
         return amounts;
     }
+
 }
